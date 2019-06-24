3b67501f267f676e85f318ce6b436729191553e7
bahrus <anderson.bruce.b@gmail.com>
2019-06-24 13:30:05 -0400

    ...

--- xtal-vlist.js
+++ xtal-vlist.js
@@ -1,126 +1,86 @@
-import { XtallatX } from 'xtal-element/xtal-latx.js';
+import { XtallatX, lispToCamel } from 'xtal-element/xtal-latx.js';
 import { hydrate } from 'trans-render/hydrate.js';
 import { define } from 'trans-render/define.js';
 import { VirtualList } from './vlist.js';
+const item_height = 'item-height';
+const total_rows = 'total-rows';
+const h = 'h';
+const w = 'w';
 export class XtalVList extends XtallatX(hydrate(HTMLElement)) {
     constructor() {
         super(...arguments);
-        // attributeChangedCallback(n: string, ov: string, nv: string){
-        //     switch(n){
-        //         case height:
-        //             this._height = parseFloat(nv);
-        //             break;
-        //         case row_count:
-        //         case initial_scroll_top:
-        //         case initial_index:
-        //         case overscan_count:
-        //         case estimated_row_height:
-        //         case row_height:
-        //             (<any>this)['_' + lispToCamel(n)] = parseFloat(nv);
-        //             break;
-        //         default:
-        //             super.attributeChangedCallback(n, ov, nv);
-        //     }
-        //     this.onPropsChange();
-        // }
         this._c = false;
-        // _renderRow!: (idx: number) => HTMLElement
-        // get renderRow(){
-        //     return this._renderRow;
-        // }
-        // set renderRow(nv){
-        //     this._renderRow = nv;
-        // }
-        // _rowHeight: rowHeightType = 200;
-        // get rowHeight(){
-        //     return this._rowHeight;
-        // }
-        // set rowHeight(nv){
-        //     this._rowHeight = nv;
-        //     this.onPropsChange();
-        // }
-        // _height: number = 500;
-        // get height(){
-        //     return this._height;
-        // }
-        // set height(nv: number){
-        //     this.attr(height, nv.toString());
-        // }
-        // _initialScrollTop = 0;
-        // get initialScrollTop(){
-        //     return this._initialScrollTop;
-        // }
-        // set initialScrollTop(nv){
-        //     this.attr(initial_scroll_top, nv.toString());
-        // }
-        // _initialIndex = 0;
-        // get initialIndex(){
-        //     return this._initialIndex;
-        // }
-        // set initialIndex(nv){
-        //     this.attr(initial_index, nv.toString());
-        // }
-        // _overscanCount = 3;
-        // get overscanCount(){
-        //     return this._overscanCount;
-        // }
-        // set overscanCount(nv){
-        //     this.attr(overscan_count, nv.toString());
-        // }
-        // _estimatedRowHeight = 20;
-        // get estimatedRowHeight(){
-        //     return this._estimatedRowHeight;
-        // }
-        // set estimatedRowHeight(nv){
-        //     this.attr(estimated_row_height, nv.toString());
-        // }
-        // _rowCount = -1;
-        // get rowCount(){
-        //     return this._rowCount;
-        // }
-        // set rowCount(nv){
-        //     this.attr(row_count, nv.toString());
-        // }
+        this._generatorFn = row => {
+            const el = document.createElement("div");
+            el.innerHTML = "<p>ITEM " + row + "</p>";
+            return el;
+        };
+        this._itemHeight = 30;
+        this._h = 600;
+        this._totalRows = -1;
     }
     static get is() { return 'xtal-vlist'; }
+    static get observedAttributes() {
+        return super.observedAttributes.concat([item_height, total_rows, h, w]);
+    }
+    attributeChangedCallback(n, ov, nv) {
+        switch (n) {
+            case total_rows:
+            case item_height:
+            case h:
+            case w:
+                ;
+                this['_' + lispToCamel(n)] = parseFloat(nv);
+                break;
+            default:
+                super.attributeChangedCallback(n, ov, nv);
+        }
+        this.onPropsChange();
+    }
     connectedCallback() {
-        // this.propUp(XtalVList.observedAttributes.map(s => lispToCamel(s)));
-        // this.propUp(['renderRow']);
+        this.style.display = 'block';
+        this.propUp(XtalVList.observedAttributes.map(s => lispToCamel(s)));
+        this.propUp(['generatorFn']);
         this._c = true;
         this.onPropsChange();
     }
-    get items() {
-        return this._items;
+    get generatorFn() {
+        return this._generatorFn;
     }
-    set items(nv) {
-        this._items = nv;
+    set generatorFn(nv) {
+        this._generatorFn = nv;
+        this.onPropsChange();
+    }
+    get itemHeight() {
+        return this._itemHeight;
+    }
+    set itemHeight(nv) {
+        this.attr(item_height, nv.toString());
+    }
+    get h() {
+        return this._h;
+    }
+    set h(nv) {
+        this.attr(h, nv.toString());
+    }
+    get totalRows() {
+        return this._totalRows;
+    }
+    set totalRows(nv) {
+        this.attr(total_rows, nv.toString());
     }
     onPropsChange() {
+        if (!this._c || !this._generatorFn || this._totalRows < 0)
+            return;
         var list = new VirtualList({
-            h: window.innerHeight,
-            itemHeight: 30,
-            totalRows: 100000,
-            generatorFn: function (row) {
-                var el = document.createElement("div");
-                el.innerHTML = "<p>ITEM " + row + "</p>";
-                return el;
-            }
+            h: this._h,
+            itemHeight: this._itemHeight,
+            totalRows: this._totalRows,
+            generatorFn: this._generatorFn,
         });
         list.container.classList.add("container");
+        this.innerHTML = '';
         this.appendChild(list.container);
-        // this.style.display = 'block';
-        // if(!this._c || this._rowCount < 0 || !this._renderRow) return;
-        // const VirtualizedList = (<any>window).VirtualizedList.default;
-        // const virtualizedList = new VirtualizedList(this, {
-        //     height: this._height, // The height of the container
-        //     rowCount: this._rowCount,
-        //     renderRow: this._renderRow,
-        //     rowHeight: this._rowHeight,
-        //     initialIndex: this._initialIndex,
-        //     initialScrollTop: this._initialScrollTop,
-        //     overscanCount: this._overscanCount,
-        //     estimatedRowHeight: this._estimatedRowHeight
-        // });
     }
 }
 define(XtalVList);
--- xtal-vlist.ts
+++ xtal-vlist.ts
@@ -3,138 +3,89 @@ import {hydrate} from 'trans-render/hydrate.js';
 import {define} from 'trans-render/define.js';
 import {VirtualList} from './vlist.js';
 
-
+const item_height = 'item-height';
+const total_rows = 'total-rows';
+const h = 'h';
+const w = 'w';
 export class XtalVList extends XtallatX(hydrate(HTMLElement)){
     static get is(){return 'xtal-vlist';}
 
-    // attributeChangedCallback(n: string, ov: string, nv: string){
-    //     switch(n){
-    //         case height:
-    //             this._height = parseFloat(nv);
-    //             break;
-    //         case row_count:
-    //         case initial_scroll_top:
-    //         case initial_index:
-    //         case overscan_count:
-    //         case estimated_row_height:
-    //         case row_height:
-    //             (<any>this)['_' + lispToCamel(n)] = parseFloat(nv);
-    //             break;
-    //         default:
-    //             super.attributeChangedCallback(n, ov, nv);
-    //     }
-    //     this.onPropsChange();
-    // }
+    static get observedAttributes(){
+        return super.observedAttributes.concat([item_height, total_rows, h, w]);
+    }
+
+    attributeChangedCallback(n: string, ov: string, nv: string){
+        switch(n){
+            case total_rows:
+            case item_height:
+            case h:
+            case w;
+                (<any>this)['_' + lispToCamel(n)] = parseFloat(nv);
+                break;
+            default:
+                super.attributeChangedCallback(n, ov, nv);
+        }
+        this.onPropsChange();
+    }
 
     _c = false;
     connectedCallback(){
-        // this.propUp(XtalVList.observedAttributes.map(s => lispToCamel(s)));
-        // this.propUp(['renderRow']);
+        this.style.display = 'block';
+        this.propUp(XtalVList.observedAttributes.map(s => lispToCamel(s)));
+        this.propUp(['generatorFn']);
         this._c = true;
         this.onPropsChange();
     }
 
-    _items!: any[];
-    get items(){
-        return this._items;
+    _generatorFn: (row: number) => HTMLElement = row =>{
+        const el = document.createElement("div");
+        el.innerHTML = "<p>ITEM " + row + "</p>";
+        return el;
     }
-    set items(nv){
-        this._items = nv;
+    get generatorFn(){
+        return this._generatorFn;
+    }
+    set generatorFn(nv){
+        this._generatorFn = nv;
+        this.onPropsChange();
+    }
+
+    _itemHeight: number = 30;
+    get itemHeight(){
+        return this._itemHeight;
+    }
+    set itemHeight(nv){
+        this.attr(item_height, nv.toString());
+    }
+
+    _h: number = 600;
+    get h(){
+        return this._h
+    }
+    set h(nv){
+        this.attr(h, nv.toString());
+    }
+    _totalRows: number = -1;
+    get totalRows(){
+        return this._totalRows;
+    }
+    set totalRows(nv){
+        this.attr(total_rows, nv.toString());
     }
 
     onPropsChange(){
+        if(!this._c || !this._generatorFn || this._totalRows < 0) return;
         var list = new VirtualList({
-            h: window.innerHeight,
-            itemHeight: 30,
-            totalRows: 100000,
-            generatorFn: function(row: any) {
-              var el = document.createElement("div");
-              el.innerHTML = "<p>ITEM " + row + "</p>";
-              return el;
-            }
+            h: this._h,
+            itemHeight: this._itemHeight,
+            totalRows: this._totalRows,
+            generatorFn: this._generatorFn,
           });
           list.container.classList.add("container");
+          this.innerHTML = '';
           this.appendChild(list.container)
-        // this.style.display = 'block';
-        // if(!this._c || this._rowCount < 0 || !this._renderRow) return;
-        // const VirtualizedList = (<any>window).VirtualizedList.default;
-        // const virtualizedList = new VirtualizedList(this, {
-        //     height: this._height, // The height of the container
-        //     rowCount: this._rowCount,
-        //     renderRow: this._renderRow,
-        //     rowHeight: this._rowHeight,
-        //     initialIndex: this._initialIndex,
-        //     initialScrollTop: this._initialScrollTop,
-        //     overscanCount: this._overscanCount,
-        //     estimatedRowHeight: this._estimatedRowHeight
-        // });
     }
 
-    // _renderRow!: (idx: number) => HTMLElement
-
-    // get renderRow(){
-    //     return this._renderRow;
-    // }
-    // set renderRow(nv){
-    //     this._renderRow = nv;
-    // }
-
-    // _rowHeight: rowHeightType = 200;
-    // get rowHeight(){
-    //     return this._rowHeight;
-    // }
-    // set rowHeight(nv){
-    //     this._rowHeight = nv;
-    //     this.onPropsChange();
-    // }
-
-    // _height: number = 500;
-    // get height(){
-    //     return this._height;
-    // }
-    // set height(nv: number){
-    //     this.attr(height, nv.toString());
-    // }
-
-    // _initialScrollTop = 0;
-    // get initialScrollTop(){
-    //     return this._initialScrollTop;
-    // }
-    // set initialScrollTop(nv){
-    //     this.attr(initial_scroll_top, nv.toString());
-    // }
-
-    // _initialIndex = 0;
-    // get initialIndex(){
-    //     return this._initialIndex;
-    // }
-    // set initialIndex(nv){
-    //     this.attr(initial_index, nv.toString());
-    // }
-
-    // _overscanCount = 3;
-    // get overscanCount(){
-    //     return this._overscanCount;
-    // }
-    // set overscanCount(nv){
-    //     this.attr(overscan_count, nv.toString());
-    // }
-
-    // _estimatedRowHeight = 20;
-    // get estimatedRowHeight(){
-    //     return this._estimatedRowHeight;
-    // }
-    // set estimatedRowHeight(nv){
-    //     this.attr(estimated_row_height, nv.toString());
-    // }
-
-    // _rowCount = -1;
-    // get rowCount(){
-    //     return this._rowCount;
-    // }
-    // set rowCount(nv){
-    //     this.attr(row_count, nv.toString());
-    // }
 
 
 
