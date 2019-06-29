1ceb6e75e9c9e6ec39559db52bb9c2efcb893470
bahrus <anderson.bruce.b@gmail.com>
2019-06-29 18:01:38 -0400

    ...

--- demo/xtal-vlist-customized.js
+++ demo/xtal-vlist-customized.js
@@ -1,12 +1,28 @@
 import { XtalVListBase } from '../xtal-vlist-base.js';
 import { define } from 'trans-render/define.js';
+import { init } from 'trans-render/init.js';
+import { createTemplate } from 'xtal-element/utils.js';
+const testTemplate = createTemplate(/* html */ `
+<div>
+    <label></label>
+</div>
+`);
 class XtalVListCustomized extends XtalVListBase {
     static get is() {
         return 'xtal-vlist-customized';
     }
     generate(row) {
         const el = document.createElement("div");
-        el.innerHTML = "<p>ITEM " + row + "</p>";
+        // el.innerHTML = "<p>ITEM " + row + "</p>";
+        // return el;
+        const ctx = {
+            Transform: {
+                div: {
+                    label: 'row' + row
+                }
+            }
+        };
+        init(testTemplate, ctx, el);
         return el;
     }
 }
--- demo/xtal-vlist-customized.ts
+++ demo/xtal-vlist-customized.ts
@@ -1,5 +1,14 @@
 import {XtalVListBase} from '../xtal-vlist-base.js';
 import {define} from 'trans-render/define.js';
+import {init} from 'trans-render/init.js';
+import {RenderContext} from 'trans-render/init.d.js';
+import {createTemplate} from 'xtal-element/utils.js';
+
+const testTemplate = createTemplate(/* html */`
+<div>
+    <label></label>
+</div>
+`);
 
 class XtalVListCustomized extends XtalVListBase{
     static get is(){
@@ -7,7 +16,16 @@ class XtalVListCustomized extends XtalVListBase{
     }
     generate(row: number){
         const el = document.createElement("div");
-        el.innerHTML = "<p>ITEM " + row + "</p>";
+        // el.innerHTML = "<p>ITEM " + row + "</p>";
+        // return el;
+        const ctx : RenderContext = {
+            Transform:{
+                div:{
+                    label: 'row' + row
+                }
+            }
+        };
+        init(testTemplate, ctx, el);
         return el;
     }
 
