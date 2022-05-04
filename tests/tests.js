20f331dd79511c59e53dc68475a9bb007b10c723
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-05-03 20:52:43 -0400

    0.0.60

--- tests/tests.js
+++ tests/tests.js
@@ -3,11 +3,11 @@ const xt = require('xtal-test/index');
     const passed = await xt.runTests([
         {
             path: 'test/xtal-editor-test1.html',
-            expectedNoOfSuccessMarkers: 1,
-            wait: 10000
-        },
+            expectedNoOfSuccessMarkers: 1
+        }
     ]);
     if (passed) {
         console.log("Tests Passed.  Have a nice day.");
     }
 })();
+
